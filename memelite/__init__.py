a0bc7c5345f175014c8c7429269bc5ef84d73560
jacob.schreiber <jacob.schreiber@clip-c2-17.cbe.vbc.ac.at>
2025-06-05 12:51:15 +0200

    v0.2.0

--- memelite/__init__.py
+++ memelite/__init__.py
@@ -1,7 +1,7 @@
 # memesuite-lite: A re-implementation of select tools from the MEME suite.
 # Author: Jacob Schreiber
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .fimo import fimo
 from .tomtom import tomtom
